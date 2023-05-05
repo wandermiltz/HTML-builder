458604875e35622afbdded4e1322763f641746ea
Olga Berezka <olga.berezka.main@gmail.com>
2023-05-05 21:47:51 +0100

    feat: improve solution for 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,10 +2,16 @@ const fs = require('fs');
 const path = require('path');
 
 const textPath = path.join(__dirname, 'text.txt');
-const readStream = fs.createReadStream(textPath);
+const readStream = fs.createReadStream(textPath, 'utf-8');
+
+let result = '';
 
 readStream.on('data', (chunk) => {
-  process.stdout.write(chunk);
+  result += chunk
+});
+
+readStream.on('end', () => {
+  process.stdout.write(result);
 });
 
 readStream.on('error', () => {
