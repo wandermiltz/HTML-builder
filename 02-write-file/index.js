d46b162bc1c4ace632886a796ec4bbf3e19a8a84
Olga Berezka <olga.berezka.main@gmail.com>
2023-05-08 22:05:47 +0100

    fix: 02-write-file text file directory path

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
 const readline = require('readline');
+const path = require('path');
 
 const textFile = 'text.txt';
+const textFilePath = path.join(__dirname, textFile);
 const messageOnStart = 'Введите текст:';
 const messageOnContinue = 'Текст записан в файл, можете продолжить ввод:'
 const messageOnEnd = 'До свидания!';
 
 // Создаем поток записи в файл
-const writeStream = fs.createWriteStream(textFile, { flags: 'a' }); //'a' - флаг append для добавления к уже существующему контенту файла
+const writeStream = fs.createWriteStream(textFilePath, { flags: 'a' }); //'a' - флаг append для добавления к уже существующему контенту файла
 
 // Выводим приветствие
 console.log(messageOnStart);
@@ -21,7 +23,7 @@ const rl = readline.createInterface({
 // Обработка введенного текста
 rl.on('line', (input) => {
   if (input === 'exit') {
-    console.log(messageOnEnd);
+    console.log(`\n${messageOnEnd}`);
     writeStream.end();
     process.exit();
   } else {
